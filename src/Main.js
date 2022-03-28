1723f64a71558443ee60a42a0a23a1cc9fa96378
Jere <35882808+jirimicvl@users.noreply.github.com>
2022-03-28 14:48:01 +0300

    commit

--- src/Main.js
+++ src/Main.js
@@ -1,55 +1,34 @@
 import './App.css';
-import React, { useState } from "react";
+import React, { Component } from "react";
 
-const Main = () => {
-    const [count, setCount] = useState(0);
-
-
-    let incrementFive = () => {
-        setCount(count + 5);
-      };
-
-  let increment = () => {
-    setCount(count + 1);
+class Main extends Component {
+  state = {
+    counter: 1000,
   };
 
-  let reset = () => {
-    setCount(0);
+  addOneHandler = ()=> {
+    console.log('wow you can really dance');
   };
 
-  let decrement = () => {
-    setCount(count - 1);
-  };
 
-  let decrementFive = () => {
-    setCount(count - 5);
-  };
-
-  
-
-    return (
+    render () {
+      return (
         <div>
             <div className="circle">
-                <div className = "current-sum">{count}</div>
+                <div className = "current-sum">{this.state.counter}</div>
             </div>
 
             <div className="button-container">
-            <button type="button" className="button" onClick={incrementFive}>Add five</button>
-            <button type="button" className="button" onClick={increment}>Add one</button>
-            <button type="button" className="button" onClick={reset}>Reset</button>
-            <button type="button" className="button" onClick={decrement}>Remove one</button>
-            <button type="button" className="button" onClick={decrementFive}>Remove five</button>
+            <button type="button" className="button" >Add five</button>
+            <button type="button" className="button">Add one</button>
+            <button type="button" className="button" >Reset</button>
+            <button type="button" className="button" >Remove one</button>
+            <button type="button" className="button"onClick={this.addOneHandler} >Remove five</button>
             </div>
 
         </div>
-
-        
-
-
-
-
-
     );
+    }
 };
 
 export default Main;
