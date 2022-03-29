9420d6f67f5a184c89cd814823cc60c3bd8af4c4
Jere <35882808+jirimicvl@users.noreply.github.com>
2022-03-29 12:01:18 +0300

    commit

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,5 +1,6 @@
 
 import React, { Component } from "react";
+import Button from "./Button";
 
 class Main extends Component {
   state = {
@@ -45,8 +46,10 @@ class Main extends Component {
             <div className="circle">
                 <div className = "current-sum">{this.state.counter}</div>
             </div>
-
+              
+            <Button onClick={this.addFiveHandler} value="5"/>
             <div className="button-container">
+            
             <button type="button" className="button" onClick={this.addFiveHandler}>Add five</button>
             <button type="button" className="button" onClick={this.addOneHandler}>Add one</button>
             <button type="button" className="button" onClick={this.resetHandler}>Reset</button>
